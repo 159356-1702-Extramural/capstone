9bf570ff1531928f018b05db722e10036d103390
Tim Ackroyd <tim@customd.com>
2017-08-06 12:21:53 +1200

    Bring Alayn’s prototype into the nodejs file structure

--- app/app.js
+++ app/app.js
@@ -7,6 +7,9 @@
 // Logging Framework
 var logger   = require('winston');
 
+// TODO: can we set this from enviroment config?
+logger.level = 'debug';
+
 // Web App Framework
 var express   = require('express');
 var app       = express();
@@ -25,32 +28,22 @@ var lobby     = new Lobby();
 // Define static files directory
 app.use(express.static('public'));
 
-// Logging example
-
-// Set logging level 
-// TODO: configure from environment variable
-logger.level = 'debug';
-
-logger.log('debug', 'Just your standard debug message.');
-logger.log('info', 'Here\'s some info', {
-    thing : 'thing-value'
-});
-logger.log('warn', 'Things are getting quite bad...');
-logger.log('error', 'Catastrophe', {
-    message : 'The end is nigh'
+// Serve static page
+app.get('/', function(req, res) {    
+    res.sendFile(__dirname + '/views/demo_index.html');
 });
 
-// Server static page
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+// Serve prototype
+app.get('/prototype', function(req, res) {
+    res.sendFile(__dirname + '/views/prototype_default.html');
 });
 
-// Handle new connection
+// Handle new socket connection
 io.on('connection', function(socket) {
-    console.log('A client has connected...');
-
+    logger.log('info', 'A client has connected...');
+    
     socket.on('join_request', function(data) {
-        console.log('Join Request');
+        logger.log('info', 'Join Request');
         lobby.assign_player(socket, data);
     });
 
@@ -61,5 +54,5 @@ io.on('connection', function(socket) {
 });
 
 server.listen(PORT, function() {
-    console.log('Listening on port: ' + PORT);
+    logger.log('info', 'Listening on port: ' + PORT);
 });
