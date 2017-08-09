4c0acbe72de67ab4f07cd97f6d7e3e37b37386dd
Craig Walker <craig@coded.co.nz>
2017-08-09 17:40:34 +1200

    SauceLabs Remote Testing ready
    Saucelabs Remote testing using Selenium is ready to test against an early version of the gameboard.
    
    TODO: reduce all functions down to a single function and an array of platforms.

--- tests/selenium.test.js
+++ tests/selenium.test.js
@@ -6,7 +6,10 @@
  * Basic automated tests to ensure functionality across all platforms
  * (Create gameplay tests for all browsers and platforms)
  * 
+ * Selenium tests currently setup for quick testing of major browsers and Desktop OS's
+ * 
  */
+var comprehensiveTest = false;
 
 var assert = require('assert');
 var SauceLabs = require("saucelabs");
@@ -19,174 +22,188 @@ var saucelabs = new SauceLabs({
 
 var test = require('ava');
 var webdriver = require('selenium-webdriver');
+var drivers = []; 
 
 /**
  * Create browser / platforms to test against here
- */
-// var driver_fx_xp = new webdriver.Builder()
-//   .withCapabilities({
-//     'browserName':'firefox',
-//     'platform':'Windows XP',
-//     'version': '42.0',})
-//   .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
-//   .build();
-
-// var driver_fx_7 = new webdriver.Builder()
-//   .withCapabilities({
-//     'browserName':'firefox',
-//     'platform':'Windows 7',
-//     'version': '46.0',})
-//   .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
-//   .build();
-
-// var driver_fx_8 = new webdriver.Builder()
-//   .withCapabilities({
-//     'browserName':'firefox',
-//     'platform':'Windows 8',
-//     'version': '50.0',})
-//   .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
-//   .build();
-
-var driver_fx_10 = new webdriver.Builder()
-  .withCapabilities({
-    'browserName':'firefox',
-    'platform':'Windows 10',
-    'version': '54.0',})
-  .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
-  .build();
-
-var driver_fx_Mac = new webdriver.Builder()
-  .withCapabilities({
-    'browserName':'firefox',
-    'platform':'Mac 10.12',
-    'version': '54.0',})
-  .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
-  .build();
-
-var driver_fx_Linux = new webdriver.Builder()
-  .withCapabilities({
-    'browserName':'firefox',
-    'platform':'Linux',
-    'version': '45.0',})
-  .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
-  .build();
-
-// var driver_chr_xp = new webdriver.Builder()
-//   .withCapabilities({
-//     'browserName':'chrome',
-//     'platform':'Windows XP',
-//     'version': '45.0',})
-//   .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
-//   .build();
-
-//   var driver_chr_7 = new webdriver.Builder()
-//   .withCapabilities({
-//     'browserName':'chrome',
-//     'platform':'Windows 7',
-//     'version': '50.0',})
-//   .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
-//   .build();
-
-// var driver_chr_8 = new webdriver.Builder()
-//   .withCapabilities({
-//     'browserName':'chrome',
-//     'platform':'Windows 8',
-//     'version': '55.0',})
-//   .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
-//   .build();
-
-var driver_chr_10 = new webdriver.Builder()
-  .withCapabilities({
-    'browserName':'chrome',
-    'platform':'Windows 10',
-    'version': '60.0',})
-  .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
-  .build();
-
-// var driver_chr_Mac = new webdriver.Builder()
-//   .withCapabilities({
-//     'browserName':'chrome',
-//     'platform':'Mac 10.12',
-//     'version': '56.0',})
-//   .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
-//   .build();
-
-// var driver_chr_Linux = new webdriver.Builder()
-//   .withCapabilities({
-//     'browserName':'chrome',
-//     'platform':'Linux',
-//     'version': '48.0',})
-//   .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
-//   .build();
-
-// var driver_sf_7 = new webdriver.Builder()
-//   .withCapabilities({
-//     'browserName':'safari',
-//     'platform':'Windows 7',
-//     'version': '5',})
-//   .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
-//   .build();
-
-// var driver_sf_Mac = new webdriver.Builder()
-//   .withCapabilities({
-//     'browserName':'safari',
-//     'platform':'Mac 10.12',
-//     'version': '10',})
-//   .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
-//   .build();
-
-// var driver_ie_xp = new webdriver.Builder()
-//   .withCapabilities({
-//     'browserName':'internet explorer',
-//     'platform':'Windows XP',
-//     'version': '8.0',})
-//   .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
-//   .build();
-
-// var driver_ie_7 = new webdriver.Builder()
-//   .withCapabilities({
-//     'browserName':'internet explorer',
-//     'platform':'Windows 7',
-//     'version': '9.0',})
-//   .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
-//   .build();
-
-// var driver_ie_8 = new webdriver.Builder()
-//   .withCapabilities({
-//     'browserName':'internet explorer',
-//     'platform':'Windows 8',
-//     'version': '10',})
-//   .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
-//   .build();
-
-var driver_ie_10 = new webdriver.Builder()
-  .withCapabilities({
-    'browserName':'internet explorer',
-    'platform':'Windows 10',
-    'version': '11',})
-  .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
-  .build();
-
-/**
- * Hold all drivers in an array.
- * Any new tests can use the array as a parameter
- * See below for example
+ * function formated driver_browser_os
+ *    i.e. driver_fx_xp = Firefox on Windows XP
+ * Note: version number coresponds to browser version number 
+ * 
+ * TODO: break down functions below to one function with an input array of platform/browers/version
  */
 
-var driversQuickTest = [driver_fx_Linux,driver_fx_Mac,driver_fx_10, driver_chr_10, driver_ie_10];
-//var driversComprehensive = [driver_chr_xp, driver_chr_7, driver_chr_8, driver_chr_10, driver_chr_Linux, driver_chr_Mac,
-//                 driver_fx_xp, driver_fx_7, driver_fx_8, driver_fx_10, driver_fx_Linux, driver_fx_Mac,
-//                 driver_ie_xp, driver_ie_7, driver_ie_8, driver_ie_10,
-//                 driver_sf_7, driver_sf_Mac];
+ function comprehensivePlatforms(){
+    var driver_fx_xp = new webdriver.Builder()
+    .withCapabilities({
+        'browserName':'firefox',
+        'platform':'Windows XP',
+        'version': '42.0',})
+    .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+    .build();
 
-var drivers = driversQuickTest;
+    var driver_fx_7 = new webdriver.Builder()
+    .withCapabilities({
+        'browserName':'firefox',
+        'platform':'Windows 7',
+        'version': '49.0',})
+    .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+    .build();
+
+    var driver_fx_8 = new webdriver.Builder()
+    .withCapabilities({
+        'browserName':'firefox',
+        'platform':'Windows 8',
+        'version': '50.0',})
+    .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+      .build();        
+
+    var driver_chr_xp = new webdriver.Builder()
+    .withCapabilities({
+        'browserName':'chrome',
+        'platform':'Windows XP',
+        'version': '45.0',})
+    .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+    .build();
+
+    var driver_chr_7 = new webdriver.Builder()
+    .withCapabilities({
+        'browserName':'chrome',
+        'platform':'Windows 7',
+        'version': '50.0',})
+    .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+    .build();
+
+    var driver_chr_8 = new webdriver.Builder()
+    .withCapabilities({
+        'browserName':'chrome',
+        'platform':'Windows 8',
+        'version': '55.0',})
+    .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+    .build();
+
+    var driver_chr_Mac = new webdriver.Builder()
+    .withCapabilities({
+        'browserName':'chrome',
+        'platform':'Mac 10.12',
+        'version': '56.0',})
+    .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+    .build();
+
+    var driver_chr_Linux = new webdriver.Builder()
+    .withCapabilities({
+        'browserName':'chrome',
+        'platform':'Linux',
+        'version': '48.0',})
+    .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+    .build();
+
+    var driver_sf_7 = new webdriver.Builder()
+    .withCapabilities({
+        'browserName':'safari',
+        'platform':'Windows 7',
+        'version': '5',})
+    .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+    .build();
+
+    var driver_sf_Mac = new webdriver.Builder()
+    .withCapabilities({
+        'browserName':'safari',
+        'platform':'Mac 10.12',
+        'version': '10',})
+    .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+    .build();
+
+    var driver_ie_xp = new webdriver.Builder()
+    .withCapabilities({
+        'browserName':'internet explorer',
+        'platform':'Windows XP',
+        'version': '8.0',})
+    .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+    .build();
+
+    var driver_ie_7 = new webdriver.Builder()
+    .withCapabilities({
+        'browserName':'internet explorer',
+        'platform':'Windows 7',
+        'version': '9.0',})
+    .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+    .build();
+
+    var driver_ie_8 = new webdriver.Builder()
+    .withCapabilities({
+        'browserName':'internet explorer',
+        'platform':'Windows 8',
+        'version': '10',})
+    .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+    .build();
+
+    return [driver_chr_xp, driver_chr_7, driver_chr_Linux, driver_chr_Mac,
+      driver_fx_xp, driver_fx_7, driver_fx_8, 
+      driver_ie_xp, driver_ie_7, driver_ie_8,
+      driver_sf_7, driver_sf_Mac];
+
+}
+function quickPlatforms(){
+  var driver_fx_10 = new webdriver.Builder()
+    .withCapabilities({
+        'browserName':'firefox',
+        'platform':'Windows 10',
+        'version': '54.0',})
+    .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+    .build();
+
+  var driver_fx_Mac = new webdriver.Builder()
+    .withCapabilities({
+        'browserName':'firefox',
+        'name': 'MacOS 10.12 - Firefox',
+        'platform':'Mac 10.12',
+        'version': '54.0',})
+    .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+    .build();
+
+  var driver_fx_Linux = new webdriver.Builder()
+    .withCapabilities({
+        'browserName':'firefox',
+        'platform':'Linux',
+        'version': '45.0',})
+    .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+    .build();
+
+  var driver_chr_10 = new webdriver.Builder()
+    .withCapabilities({
+        'browserName':'chrome',
+        'platform':'Windows 10',
+        'version': '60.0',})
+    .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+    .build();
+
+  var driver_ie_10 = new webdriver.Builder()
+    .withCapabilities({
+        'browserName':'internet explorer',
+        'platform':'Windows 10',
+        'version': '11',})
+    .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+    .build();
+
+  var driver_edge_10 = new webdriver.Builder()
+    .withCapabilities({
+        'browserName':'microsoftedge',
+        'platform':'Windows 10',
+        'version': '15',})
+    .usingServer('http://' + username + ':' + accessKey + '@ondemand.saucelabs.com:80/wd/hub')
+    .build();
+
+    return [driver_fx_Linux,driver_fx_Mac,driver_fx_10, driver_chr_10, driver_ie_10, driver_edge_10];
+}
+
 /**
  * Execute tests here
  * Write functions for each test here and call them in the section below
  */
 
 function frontPageLoads(browserDriver){
-  test('Front game page exists ('+browserDriver.browserName+" / "+browserDriver.platform+")", async t => {
+  test('Front game page exists (todo:add browser/os here)', async t => {
     let driver = browserDriver;
     await driver.get('http://capstone-settlers.herokuapp.com/');
     t.is(await driver.getTitle(), "Settlers of Massey");
@@ -194,16 +211,28 @@ function frontPageLoads(browserDriver){
 });
 }
 
+/**
+ * Drivers and platform/browser combos enabled here
+ */
 
+if(comprehensiveTest){
+  //create drivers using both the quick tests and the outlier tests
+  drivers = quickPlatforms().concat(comprehensivePlatforms());
+}else{
+  drivers = quickPlatforms();
+}
 
 /**
  * Call tests here
  */
+
 for(var i = 0; i < drivers.length; i++){
+//  var concurrentDrivers = 0;
+//  while(concurrentDrivers > 5){
+//    saucelabs.getJobs(function(err,jobs){
+//      concurrentDrivers = jobs.length;
+//    })
+//   }
   frontPageLoads(drivers[i]);
 }
-
-test.todo('Test add a player');
-
-
 
