62c2b344074af92a52b8daef1c03a7edebe812a3
Tim Ackroyd <tim@customd.com>
2017-08-11 13:04:22 +1200

    Allow players to join game
    Allows players to join, players can select the play button on the start modal where they will be asked to join a game. They will then be added to a game queue. When the game is full the start modal will close.

--- public/js/game/setup.js
+++ public/js/game/setup.js
@@ -3,20 +3,16 @@ var socket = io();
 
 $(document).ready(function() {
 
-    var $doc = $(document);
+  var $doc = $(document);
     
-    // Start Menu section toggler
+    // Start menu section links
     $doc.on('click', '.js-start-toggle', function(e) {
         e.preventDefault();
         var active_class = $(this).attr('data-target');
-
-        $('.start .start_subsection').each(function() {
-            var $section = $(this);
-            $section.toggleClass('hide', !$section.hasClass(active_class));
-        });
+        display_start_modal(active_class);
     });
 
-    // Join a Game
+    // Request to join a game
     $doc.on('click', '.js-start-game', function() {
         var name = $('#txt_player1').val();
         if (name == '') {
@@ -29,10 +25,36 @@ $(document).ready(function() {
             name: name
         });
         
-        // TODO: display spinner and message "Waiting for more players to join..."
+        display_start_modal('waiting');
 
     });
 
-    // TODO: detect when game is full and close the waiting spinner
+    // Uodate the waiting display as new players join the game
+    socket.on('player_joined', function (data) {
+        $('.start .waiting_text')
+            .find('.count').text(data.player_count)
+            .end()
+            .find('.total').text(data.max_players);
+    });
+
+    // Detect the game starting
+    socket.on('game_start', function (data) {
+        $('.start').fadeOut(400, function() {
+            // TODO: Get the game started!
+            alert('Let the game begin!');
+        });
+    });
+
+    /**
+     * Displays a subsection of the start modal
+     * 
+     * @param {string} active_class : subsection class that you want to display in the start modal
+     */
+    function display_start_modal(active_class) {
+        $('.start .start_subsection').each(function () {
+            var $section = $(this);
+            $section.toggleClass('hide', !$section.hasClass(active_class));
+        });
+    }
 
 });
