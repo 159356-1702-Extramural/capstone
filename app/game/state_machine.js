715102f538daf2424187c2dcb6fb4c00262df8c2
Tim Ackroyd <tim@customd.com>
2017-08-22 13:29:09 +1200

    Fixes issues with start up resource allocation
    - fixes some incorrectly named variables
    - updated the interface with resource cards
    - fixbug causing game update to be sent before the resource cards were allocated

--- app/game/state_machine.js
+++ app/game/state_machine.js
@@ -113,19 +113,18 @@ StateMachine.prototype.tick = function(data) {
             this.send_to_player('game_update', data_package);
         }
 
-        //call start sequence again from here - startSequence will find the next player to have a turn
-        this.game_start_sequence();
-        this.broadcast_gamestate();
-
         this.game.players[data.player_id].turn_complete = true;
         this.game.players[data.player_id].turn_data = data;
         //logger.log('debug', 'Player '+data.player_id+' has tried to place a settlement.');
         //distribute resources from the second round settlement placement
 
-        if(this.setupPointer - 1 > this.setupSequence.length / 2){
+        if(this.setupPointer > this.setupSequence.length / 2){
             this.game.secondRoundResources(this.game.players[data.player_id], data);
         }
 
+        //call start sequence again from here - startSequence will find the next player to have a turn
+        this.game_start_sequence();
+        this.broadcast_gamestate();
 
         // For now: increment round number and reset the player turn
         // completion status
