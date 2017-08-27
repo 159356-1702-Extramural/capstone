56b403f996d5750d14da1cf8e9e78be9c25ff043
Tim Ackroyd <tim@customd.com>
2017-08-28 08:46:37 +1200

    Additional testing

--- tests/games.test.js
+++ tests/games.test.js
@@ -32,3 +32,25 @@ test("Reset games to clean state", function(t) {
     g.hard_reset();
     t.is(g.games.length, 0);
 });
+
+test("Player can be assigned to a game", function(t) {
+  var g = new games.Games();
+
+  var mockSocket = {
+    emit: function() { },
+    on: function() {}
+  };
+
+  g.assign_player(mockSocket, {name: 'Tim'});
+
+  t.is(g.games.length, 1);
+
+  g.assign_player(mockSocket, { name: 'Paul' });
+  g.assign_player(mockSocket, { name: 'John' });
+  g.assign_player(mockSocket, { name: 'George' });
+  g.assign_player(mockSocket, { name: 'Ringo' });
+
+  // HACK: cuurent game maxsiz it 2, when this is changed to 4 this test
+  // will ned to be changed
+  t.is(g.games.length, 3);
+});
