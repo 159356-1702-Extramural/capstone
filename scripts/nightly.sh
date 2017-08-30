0e638897e5da8c3a992a9b54fa0ead639fe6391c
Craig Walker <craig@coded.co.nz>
2017-08-30 22:45:31 +1200

    Testing Sauce connect to  slack

--- scripts/nightly.sh
+++ scripts/nightly.sh
@@ -8,9 +8,11 @@ set -ev
 #    + returns 'cron' when called by the travis cron job trigger
 
 if [ "${TRAVIS_EVENT_TYPE}" = "push" ]; then
+        npm test;
+        echo "running selenium headless browser tests";
         pip install selenium && pip install sauceclient
         python tests/seleniumPythonTest/click_tutorial.py;
-        echo "running selenium headless browser tests";
+        
     else      
         npm test;
 fi
