aac0affd54aad9023cca5ee4e5b7798f25362560
Jason Qu <fishinsea2014@gmail.com>
2017-09-03 11:01:25 +1200

    Go on 12

--- tests/seleniumPythonTest/run_all_tests.py
+++ tests/seleniumPythonTest/run_all_tests.py
@@ -7,14 +7,14 @@ import os, time
 # tests/seleniumPythonTest/run_all_tests.py
 
 #This is for test
-p=os.path.abspath('run_all_tests.py')
-test_dir,nm=os.path.split(p)
-print test_dir
+# p=os.path.abspath('package.json')
+# test_dir,nm=os.path.split(p)
+# print test_dir
 
-# test_dir= os.path.dirname(os.path.abspath('.'))
+test_dir= os.path.dirname(os.path.abspath('.'))
 
-test_case_dir=test_dir+'/test_case/'
-test_reports_dir=test_dir+'/test_reports/'
+test_case_dir=test_dir+'\\test_case\\'
+test_reports_dir=test_dir+'\\selenium_test_reports\\'
 print test_reports_dir
 def creatsuite():
     testunit=unittest.TestSuite()
@@ -32,6 +32,9 @@ def creatsuite():
     return testunit
 alltestnames = creatsuite()
 now = time.strftime('%m-%d-%Y-%H_%M_%S',time.localtime(time.time()))
+#If selenium test reports dir is not exists, then create it.
+if not os.path.isdir(test_reports_dir):
+    os.mkdir(test_reports_dir)
 
 filename = test_reports_dir+now+'result.html'
 # filename = test_dir+'//'+now+'result.html'
