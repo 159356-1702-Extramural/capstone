7d01a0196d2dd7f71eee58ea83bd776c4a0b7468
Jason Qu <fishinsea2014@gmail.com>
2017-09-03 12:08:44 +1200

    Finished, code is clean. Pls find the selenium test report in "selenium_test_reports" dir.

--- tests/seleniumPythonTest/run_all_tests.py
+++ tests/seleniumPythonTest/run_all_tests.py
@@ -4,14 +4,6 @@ import HTMLTestRunner
 import os,sys, time
 
 # Directory for locating test cases
-# tests/seleniumPythonTest/run_all_tests.py
-
-#This is for test
-# p=os.path.abspath('run_all_tests.py')
-# test_dir,nm=os.path.split(p)
-# print test_dir
-
-# test_dir= os.path.dirname(os.path.abspath('run_all_tests.py'))
 test_dir=os.path.split(os.path.realpath(sys.argv[0]))[0]
 test_case_dir=test_dir+'\\test_case\\'
 test_reports_dir=test_dir+'\\selenium_test_reports\\'
@@ -37,12 +29,13 @@ if not os.path.isdir(test_reports_dir):
     os.mkdir(test_reports_dir)
 
 filename = test_reports_dir+now+'result.html'
-# filename = test_dir+'//'+now+'result.html'
 fp = file(filename, 'wb')
 runner =HTMLTestRunner.HTMLTestRunner(
     stream=fp,
     title=u'Settler of Massey Selenium Test Report',
     description=u'Results of the tests:')
+
+# runner=unittest.TextTestRunner(); #Run this line when only want to see result on screen.
 
 #Running the test cases.
 runner.run(alltestnames)
