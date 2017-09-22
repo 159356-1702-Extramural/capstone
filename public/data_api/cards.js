24973c69ef21d94391ae005e4925e30df95a34e1
Alayn Clint <alayn@clints.com>
2017-09-22 13:35:01 +1200

    Reset card testing options

--- public/data_api/cards.js
+++ public/data_api/cards.js
@@ -3,11 +3,11 @@
  */
 function Cards(){
     this.resource_cards = {
-        brick : 2,
-        grain : 3,
-        sheep : 1,
-        lumber: 2,
-        ore   : 3
+        brick : 0,
+        grain : 0,
+        sheep : 0,
+        lumber: 0,
+        ore   : 0
     };
 
     this.dev_cards = {
