1f29ce8a63d037014888ccefc0ad117b6b0ace6e
Jason Qu <fishinsea2014@gmail.com>
2017-09-23 23:36:12 +1200

    Set the test locally or on the saucelab

--- tests/seleniumPythonTest/test_case/slnm_test_two_players_play.py
+++ tests/seleniumPythonTest/test_case/slnm_test_two_players_play.py
@@ -4,9 +4,7 @@ import sys
 import new
 import unittest
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
 from sauceclient import SauceClient
-from threading import Thread
 import time
 
 
@@ -32,23 +30,27 @@ def on_platforms(platforms):
             module[name] = new.classobj(name, (base_class,), d)
     return decorator
 
-@on_platforms(browsers)
+
+# @on_platforms(browsers) #Comment this line when do the test locally
 class SettlerSeleniumTest(unittest.TestCase):
     def setUp(self):
-        self.desired_capabilities['name'] = self.id()
-        sauce_url = "http://%s:%s@ondemand.saucelabs.com:80/wd/hub"
-        self.driver = webdriver.Remote(
-            desired_capabilities=self.desired_capabilities,
-            command_executor=sauce_url % (USERNAME, ACCESS_KEY)
-        )
 
-        self.driver_player2=webdriver.Remote(
-            desired_capabilities=self.desired_capabilities,
-            command_executor=sauce_url % (USERNAME, ACCESS_KEY)
-        )
+        #This is for saucelab test, comment this fragment of codes when test locally
+        # self.desired_capabilities['name'] = self.id()
+        # sauce_url = "http://%s:%s@ondemand.saucelabs.com:80/wd/hub"
+        # self.driver = webdriver.Remote(
+        #     desired_capabilities=self.desired_capabilities,
+        #     command_executor=sauce_url % (USERNAME, ACCESS_KEY)
+        # )
+        #
+        # self.driver_player2=webdriver.Remote(
+        #     desired_capabilities=self.desired_capabilities,
+        #     command_executor=sauce_url % (USERNAME, ACCESS_KEY)
+        # )
 
-
-        # self.driver=webdriver.Firefox()
+        #This is for locally test, comment this fragment of codes when test on saucelab
+        self.driver=webdriver.Firefox()
+        self.driver_player2=webdriver.Firefox()
 
 
     def test_game_start(self):
@@ -81,7 +83,6 @@ class SettlerSeleniumTest(unittest.TestCase):
         print wd.find_element_by_css_selector("div.btn.btn-info").text
         get_start.click()
 
-        driver_player2.quit()
 
         # wd.find_element_by_class_name("btn btn-info").click()
         self.assertTrue(success)
@@ -89,18 +90,21 @@ class SettlerSeleniumTest(unittest.TestCase):
 
 
     def tearDown(self):
-        print("Link to your job: https://saucelabs.com/jobs/%s" % self.driver.session_id)
-        try:
-            if sys.exc_info() == (None, None, None):
-                sauce.jobs.update_job(self.driver.session_id, passed=True)
-                print "Test passed, sessionId: %s" %self.driver.session_id
-            else:
-                sauce.jobs.update_job(self.driver.session_id, passed=False)
-                print "Test failed, sessionId: %s" %self.driver.session_id
-        finally:
-            self.driver.quit()
-            self.driver_player2.quit()
-
+        #Comment this fragment of codes when test locally
+        # print("Link to your job: https://saucelabs.com/jobs/%s" % self.driver.session_id)
+        # try:
+        #     if sys.exc_info() == (None, None, None):
+        #         sauce.jobs.update_job(self.driver.session_id, passed=True)
+        #         print "Test passed, sessionId: %s" %self.driver.session_id
+        #     else:
+        #         sauce.jobs.update_job(self.driver.session_id, passed=False)
+        #         print "Test failed, sessionId: %s" %self.driver.session_id
+        # finally:
+        #     self.driver.quit()
+        #     self.driver_player2.quit()
+        #Comment this fragment of codes when test on saucelab.
+        self.driver.quit()
+        self.driver_player2.quit()
 
 if __name__ == '__main__':
     unittest.main()
