8b311948682efc0591ba17d00bb47bf7a24dcccb
Luke Jones <luke.nukem.jones@gmail.com>
2017-09-29 09:25:19 +1300

    formatting: run through js-beautifier

--- app/data_api/player.js
+++ app/data_api/player.js
@@ -9,37 +9,44 @@ var Cards = require('../../public/data_api/cards.js');
  * @param {any} data
  */
 function Player(socket, data) {
-    this.id             = null;
-    this.name           = data.name;
-    this.game_id        = data.game_id;
-    this.game_name      = data.game_name;
-    this.socket         = socket;
+  this.id = null;
+  this.name = data.name;
+  this.game_id = data.game_id;
+  this.game_name = data.game_name;
+  this.socket = socket;
 
-    // True when player has won the game
-    this.winner         = false;
+  // True when player has won the game
+  this.winner = false;
 
-    this.turn_complete  = false;
-    //hold on to turn data : null was causing errors with .length
-    this.turn_data      = [];
+  this.turn_complete = false;
+  //hold on to turn data : null was causing errors with .length
+  this.turn_data = [];
 
-    // Players colour
-    this.colour         = null;
+  // Players colour
+  this.colour = null;
 
-    this.score          = {
-      total_points   : 0,
-      victory_points : 0,
-      road_length    : 0,
-      settlements    : 0,
-      cities         : 0,
-      longest_road   : false,
-      largest_army   : false
-    };
+  this.score = {
+    total_points: 0,
+    victory_points: 0,
+    road_length: 0,
+    settlements: 0,
+    cities: 0,
+    longest_road: false,
+    largest_army: false
+  };
 
-    this.actions        = [];
-    this.cards          = new Cards();
-    this.round_distribution_cards = new Cards();
+  this.actions = [];
+  this.cards = new Cards();
+  this.round_distribution_cards = new Cards();
 
-    this.trading = { sheep:false, ore:false, lumber:false, brick:false, grain:false, three:false };
+  this.trading = {
+    sheep: false,
+    ore: false,
+    lumber: false,
+    brick: false,
+    grain: false,
+    three: false
+  };
 }
 
 module.exports = Player;
--- tests/games.test.js
+++ tests/games.test.js
@@ -7,12 +7,15 @@ var games = require('../app/game/games.js');
 var sm = require('../app/game/state_machine.js');
 
 var g;
-var player = { name:"Test Player", game_name:"TEST"}
+var player = {
+  name: "Test Player",
+  game_name: "TEST"
+}
 var mocket = {
-  emit: function(d) {
+  emit: function (d) {
     if (d == "game_full") game_full = true;
   },
-  on: function() { }
+  on: function () {}
 };
 var game_full = false;
 
@@ -21,43 +24,55 @@ test.beforeEach(t => {
   game_full = false;
 });
 
-test("Create new game with player", function(t) {
+test("Create new game with player", function (t) {
   g.new_game(mocket, player, 4);
   t.is(g.games.length, 1);
   t.is(g.games[0].game.name, "TEST");
 });
 
-test("Reset games to clean state", function(t) {
+test("Reset games to clean state", function (t) {
   g.new_game(mocket, player, 4);
   t.is(g.games.length, 1);
   g.hard_reset();
   t.is(g.games.length, 0);
 });
 
-test("New players can be assigned to a game except when game is full", function(t) {
+test("New players can be assigned to a game except when game is full", function (t) {
   g.new_game(mocket, player, 4);
   t.is(g.games.length, 1);
 
-  g.assign_player(mocket, { name: 'Paul', game_id: 0 });
-  g.assign_player(mocket, { name: 'John', game_id: 0 });
-  g.assign_player(mocket, { name: 'George', game_id: 0 });
+  g.assign_player(mocket, {
+    name: 'Paul',
+    game_id: 0
+  });
+  g.assign_player(mocket, {
+    name: 'John',
+    game_id: 0
+  });
+  g.assign_player(mocket, {
+    name: 'George',
+    game_id: 0
+  });
   t.false(game_full);
-  g.assign_player(mocket, { name: 'Ringo', game_id: 0 });
+  g.assign_player(mocket, {
+    name: 'Ringo',
+    game_id: 0
+  });
   t.true(game_full);
   t.is(g.games.length, 1);
 });
 
-test("Player assigned without testing data", function(t) {
+test("Player assigned without testing data", function (t) {
   g.new_game(mocket, player, 4);
   t.is(g.games.length, 1);
 
   t.is(g.games[0].state, 'setup');
   t.is(g.games[0].setupComplete, false);
   t.is(g.games[0].setupPointer, 0);
-  t.is(g.games[0].game.round_num,1);
+  t.is(g.games[0].game.round_num, 1);
 });
 
-test("Player assigned with testing data", function(t) {
+test("Player assigned with testing data", function (t) {
   // environment variables set
   process.env.setup = 'skip';
 
@@ -68,10 +83,10 @@ test("Player assigned with testing data", function(t) {
   // TODO: test items on board to be sure
   t.is(g.games[0].setupComplete, true);
   t.is(g.games[0].setupPointer, 8);
-  t.is(g.games[0].game.round_num,3);
+  t.is(g.games[0].game.round_num, 3);
 });
 
-test("Player assigned with testing data", function(t) {
+test("Player assigned with testing data", function (t) {
   // environment variables set
   process.env.setup = 'skip';
   process.env.players = 4;
@@ -86,5 +101,5 @@ test("Player assigned with testing data", function(t) {
   // TODO: test items on board to be sure
   t.is(g.games[0].setupComplete, true);
   t.is(g.games[0].setupPointer, 8);
-  t.is(g.games[0].game.round_num,3);
+  t.is(g.games[0].game.round_num, 3);
 });
