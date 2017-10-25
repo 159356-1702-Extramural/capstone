ecdb06479055df0876afbe27c370ba6a21fe9faf
Craig Walker <craig@coded.co.nz>
2017-10-25 18:19:54 +1300

    needs testing

--- tests/seleniumPythonTest/test_case/slnm_test_gameplay.py
+++ tests/seleniumPythonTest/test_case/slnm_test_gameplay.py
@@ -15,38 +15,228 @@ USERNAME = "sumnerfit"
 ACCESS_KEY = "e8a11001-6685-43c4-901b-042e862a93f4"
 sauce = SauceClient(USERNAME, ACCESS_KEY)
 arrayPointer = 0
-desired_caps = [
-        {'browserName':'firefox',
-        'platform':'Windows 10',
-        'screenResolution' : '1280x1024',
-        'name': 'Windows 10 - Firefox',
-        'screenResolution' : '1280x1024',
-        'version': '54.0',},
-        { 'browserName':'firefox',
-        'platform':'Linux',
-        'name': 'Linux - Firefox',
-        'screenResolution' : '1024x768',
-        'version': '45.0',},
-        {'browserName':'chrome',
-        'platform':'Windows 10',
-        'name': 'Windows 10 - Chrome',
-        'screenResolution' : '1280x1024',
-        'version': '60.0',},
-        {'browserName':'internet explorer',
-        'platform':'Windows 10',
-        'name': 'Windows 10 - Internet Explorer',
-        'screenResolution' : '1280x1024',
-        'version': '11',},
-        {'browserName':'microsoftedge',
-        'platform':'Windows 10',
-        'name': 'Windows 10 - Microsoft Edge',
-        'screenResolution' : '1280x1024',
-        'version': '15',},
-        {'browserName':'firefox',
-        'name': 'MacOS 10.12 - Firefox',
-        'screenResolution' : '1024x768',
-        'platform':'Mac 10.12',
-        'version': '54.0',}]
+# desired_caps = [
+#         {'browserName':'firefox',
+#         'platform':'Windows 10',
+#         'screenResolution' : '1280x1024',
+#         'name': 'Windows 10 - Firefox',
+#         'screenResolution' : '1280x1024',
+#         'version': '54',},
+#         { 'browserName':'firefox',
+#         'platform':'Linux',
+#         'name': 'Linux - Firefox',
+#         'screenResolution' : '1024x768',
+#         'version': '45.0',},
+#         {'browserName':'chrome',
+#         'platform':'Windows 10',
+#         'name': 'Windows 10 - Chrome',
+#         'screenResolution' : '1280x1024',
+#         'version': '60.0',},
+#         {'browserName':'internet explorer',
+#         'platform':'Windows 10',
+#         'name': 'Windows 10 - Internet Explorer',
+#         'screenResolution' : '1280x1024',
+#         'version': '11',},
+#         {'browserName':'microsoftedge',
+#         'platform':'Windows 10',
+#         'name': 'Windows 10 - Microsoft Edge',
+#         'screenResolution' : '1280x1024',
+#         'version': '15',},
+#         {'browserName':'firefox',
+#         'name': 'MacOS 10.12 - Firefox',
+#         'screenResolution' : '1024x768',
+#         'platform':'Mac 10.12',
+#         'version': '54.0',}]
+
+browserVariations = [{'platform': 'Windows 10',
+'browserName':'firefox',
+'startVersion': 43,
+'endVersion': 55
+},
+{'platform': 'Windows 10',
+'browserName':'chrome',
+'startVersion': 26,
+'endVersion': 60
+},
+{'platform': 'Windows 10',
+'browserName':'ie',
+'startVersion': 11,
+'endVersion': 11
+},
+{'platform': 'Windows 10',
+'browserName':'edge',
+'startVersion': 13,
+'endVersion': 15
+},
+{'platform':'Windows 8.1',
+'browserName':'firefox',
+'startVersion': 4,
+'endVersion': 55
+},
+{'platform':'Windows 8.1',
+'browserName':'chrome',
+'startVersion': 26,
+'endVersion': 60
+},
+{'platform':'Windows 8.1',
+'browserName':'ie',
+'startVersion': 11,
+'endVersion': 11
+},
+{'platform':'Windows 8',
+'browserName':'firefox',
+'startVersion': 4,
+'endVersion': 55
+},
+{'platform':'Windows 8',
+'browserName':'chrome',
+'startVersion': 26,
+'endVersion': 60
+},
+{'platform':'Windows 8',
+'browserName':'ie',
+'startVersion': 10,
+'endVersion': 10
+},
+{'platform':'Windows 8',
+'platform':'Windows 7',
+'browserName':'firefox',
+'startVersion': 4,
+'endVersion': 55
+},
+{'platform':'Windows 8',
+'browserName':'chrome',
+'startVersion': 26,
+'endVersion': 60
+},
+{'platform':'Windows 8',
+'browserName':'ie',
+'startVersion': 8,
+'endVersion': 11
+},
+{'platform':'Windows 8',
+'browserName':'opera',
+'startVersion': 11,
+'endVersion': 12
+},
+{'platform':'Windows 8',
+'browserName':'safari',
+'startVersion': 5,
+'endVersion': 5
+},
+{'platform':'Windows XP',
+'browserName':'firefox',
+'startVersion': 4,
+'endVersion': 45
+},
+{'platform':'Windows XP',
+'browserName':'chrome',
+'startVersion': 26,
+'endVersion': 49
+},
+{'platform':'Windows XP',
+'browserName':'ie',
+'startVersion': 6,
+'endVersion': 8
+},
+{'platform':'Windows XP',
+'browserName':'opera',
+'startVersion': 11,
+'endVersion': 12
+},
+{'platform':'Mac 10.12',
+'browserName':'firefox',
+'startVersion': 4,
+'endVersion': 45
+},
+{'platform':'Mac 10.12',
+'browserName':'chrome',
+'startVersion': 27,
+'endVersion': 60
+},
+{'platform':'Mac 10.12',
+'browserName':'safari',
+'startVersion': 10,
+'endVersion': 10
+},
+{'platform':'Mac 10.11',
+'browserName':'firefox',
+'startVersion': 4,
+'endVersion': 45
+},
+{'platform':'Mac 10.11',
+'browserName':'chrome',
+'startVersion': 27,
+'endVersion': 60
+},
+{'platform':'Mac 10.11',
+'browserName':'safari',
+'startVersion': 9,
+'endVersion': 10
+},
+{'platform':'Mac 10.10',
+'browserName':'chrome',
+'startVersion': 37,
+'endVersion': 60
+},
+{'platform':'Mac 10.10',
+'browserName':'safari',
+'startVersion': 8,
+'endVersion': 8
+},
+{'platform':'Mac 10.9',
+'browserName':'firefox',
+'startVersion': 4,
+'endVersion': 55
+},
+{'platform':'Mac 10.9',
+'browserName':'chrome',
+'startVersion': 31,
+'endVersion': 60
+},
+{'platform':'Mac 10.9',
+'browserName':'safari',
+'startVersion': 7,
+'endVersion': 7
+},
+{'platform':'Mac 10.8',
+'browserName':'firefox',
+'startVersion': 4,
+'endVersion': 48
+},
+{'platform':'Mac 10.8',
+'browserName':'chrome',
+'startVersion': 27,
+'endVersion': 49
+},
+{'platform':'Mac 10.8',
+'browserName':'safari',
+'startVersion': 6,
+'endVersion': 6
+},
+{'platform':'Linux',
+'browserName':'firefox',
+'startVersion': 4,
+'endVersion': 45
+},
+{'platform':'Linux',
+'browserName':'chrome',
+'startVersion': 26,
+'endVersion': 48
+},
+{'platform':'Linux',
+'browserName':'opera',
+'startVersion': 12,
+'endVersion': 12
+}]
+
+desired_caps = []
+
+def build_desired_caps():
+  global desired_caps
+  for cap in browserVariations:
+    for i in reversed(range(cap['startVersion'], cap['endVersion'])):
+      desired_caps.append({'browserName':cap['browserName'],'platform':cap['platform'],'name': "Game items test | " + cap['platform'] + ' - ' + cap['browserName'] + ' - ' + str(i),'version': i})
 
 def get_desired_cap(desired_cap):
   USERNAME = "sumnerfit"
@@ -81,7 +271,8 @@ def buy_dev_card():
     print "start up first player"
     # add name to input
     playerInput = driverA.find_element_by_id('player-input')
-    playerInput.send_keys(desired_caps[arrayPointer]['browserName']+"-"+desired_caps[arrayPointer]['version']+"-p1")
+    print desired_caps[arrayPointer]['browserName']+"-" + str(desired_caps[arrayPointer]['version']) + "-p1"
+    playerInput.send_keys(desired_caps[arrayPointer]['browserName']+"-" + str(desired_caps[arrayPointer]['version']) + "-p1")
     start_game = driverA.find_element_by_id('start-game')
     start_game.click()
     driverA.find_element_by_id('start-2-players').click()
@@ -97,7 +288,7 @@ def buy_dev_card():
     print "click game start"
     driverB.implicitly_wait(10)
 
-    gameString = desired_caps[arrayPointer]["browserName"]+ "-" +desired_caps[arrayPointer]["version"]
+    gameString = desired_caps[arrayPointer]["browserName"]+ "-" + str(desired_caps[arrayPointer]["version"])
     print gameString
     game_to_start = driverB.find_element_by_xpath('//div[contains(text(), "' + gameString + '-p1\'s Game")]')
     game_to_start.click()
@@ -112,9 +303,11 @@ def buy_dev_card():
     driverA.find_element_by_id('begin-round-btn').click()
     driverB.find_element_by_id('begin-round-btn').click()
 
+    print 'buy dev card'
     buybutton = driverA.find_elements_by_class_name('buybutton')
     buybutton[0].click()
 
+    print ' try to play road building card'
     road_building = driverA.find_elements_by_class_name('road_building')
     road_building[0].click()
     road_building_close = driverA.find_elements_by_class_name('btn-large')
@@ -131,15 +324,28 @@ def buy_dev_card():
     chat_message_A =  driverA.find_elements_by_class_name('chat_message')
     chat_message_B =  driverA.find_elements_by_class_name('chat_message')
 
+    print 'finished tests'
     finish_testing(driverA)
     finish_testing(driverB)
     arrayPointer = arrayPointer + 1
-    buy_dev_card()
 
   except:
-    sauce.jobs.update_job(driverA.session_id, passed=False)
-    sauce.jobs.update_job(driverB.session_id, passed=False)
-    print "Test failed, sessionId: %s" %driverA.session_id
+    with open("tests/seleniumPythonTest/test_case/headless_results.txt", "a") as myfile:
+      myfile.write("\nFAILED:  - " + desired_caps[arrayPointer]['platform'] + " - " + desired_caps[arrayPointer]['browserName'] + " - " + str(desired_caps[arrayPointer]['version']))
+      myfile.write("\n     -Test results here:  https://saucelabs.com/jobs/%s" % driverA.session_id  )
+      myfile.write("\n     -Test results here:  https://saucelabs.com/jobs/%s" % driverB.session_id  )
+      myfile.close()
+    if driverA:
+      sauce.jobs.update_job(driverA.session_id, passed=False)
+      print "Test failed, sessionId: %s" %driverA.session_id
+      driverA.quit()
+    if driverB:
+      sauce.jobs.update_job(driverB.session_id, passed=False)
+      print "Test failed, sessionId: %s" %driverB.session_id
+      driverB.quit()
+    
+    arrayPointer = arrayPointer + 1
+    buy_dev_card()
 
 def finish_testing(driver):
   print "Link to your job: https://saucelabs.com/jobs/%s" % driver.session_id
@@ -153,4 +359,6 @@ def finish_testing(driver):
   finally:
     driver.quit()
 
-buy_dev_card()
+build_desired_caps()
+for cap in desired_caps:
+  buy_dev_card()
